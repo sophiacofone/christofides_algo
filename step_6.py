118537a4f43442a61804c371ebbeb2c30f4f1936
rblalani <113467189+rblalani@users.noreply.github.com>
2022-12-09 18:22:19 -0500

    Update step_6.py

--- step_6.py
+++ step_6.py
@@ -1,5 +1,20 @@
 from step_5_find_eulerian_tour import find_eulerian_tour
 
+'''
+Input: Eulerian Circuit
+
+Action:
+    For each vertex in the Circuit
+        if the vertex is not in the list removed edges
+            add it to the list approx_optimal_path
+            add it to the list removed edges
+            
+Output: 
+A list of verticies forming a Hamiltonian Circut
+        
+
+'''
+
 def convert_Eulerian_Circuit(eulerian_Circut): 
     approx_optimial_path = []
     removed_edges = []
@@ -15,4 +30,7 @@ def main():
     
     hamiltonian_Circut = convert_Eulerian_Circuit(eulerian_circut)
     print(hamiltonian_Circut)
+    
+if __name__ == '__main__':
+    main()
     
