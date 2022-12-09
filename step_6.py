2167ba815c77d802571f7bd0cab5f5c0c11935af
rblalani <113467189+rblalani@users.noreply.github.com>
2022-12-08 21:24:08 -0500

    Update step_6.py

--- step_6.py
+++ step_6.py
@@ -2,7 +2,7 @@ def Eulerian_path():
     euler_path = []
     removed_edges = []
 
-    for num in ep_with_copies:
-        if num not in blacklist:
+    for num in euler_path:
+        if num not in removed_edges:
             euler_path.append(num)
             removed_edges.append(num)
