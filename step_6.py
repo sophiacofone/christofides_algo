3cf14caf0c6b02362af932cb0b50c06ed7427408
rblalani <113467189+rblalani@users.noreply.github.com>
2022-12-09 18:14:47 -0500

    Update step_6.py

--- step_6.py
+++ step_6.py
@@ -4,16 +4,15 @@ def convert_Eulerian_Circuit(eulerian_Circut):
     approx_optimial_path = []
     removed_edges = []
 
-    for vertex in euler_path:
+    for vertex in eulerian_Circut:
         if vertex not in removed_edges:
-            euler_path.append(num)
-            removed_edges.append(num)
+            approx_optimial_path.append(vertex)
+            removed_edges.append(vertex)
     return approx_optimial_path
 
 def main(): 
     eulerian_circut = [1, 0, 4, 1, 0, 4, 1, 2, 1, 3, 5, 2, 5, 3, 1]
     
-    hamiltonian_Circut = convert_Eulerian_Circuit(eulerian_Circut)
-    print(hh=amiltonian_Circut
+    hamiltonian_Circut = convert_Eulerian_Circuit(eulerian_circut)
+    print(hamiltonian_Circut)
     
-   
