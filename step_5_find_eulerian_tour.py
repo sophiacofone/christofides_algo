30249d224562e80eeb02109ed177c8960443c88a
rblalani <113467189+rblalani@users.noreply.github.com>
2022-12-09 17:32:50 -0500

    Update step_5_find_eulerian_tour.py

--- step_5_find_eulerian_tour.py
+++ step_5_find_eulerian_tour.py
@@ -1,15 +1,5 @@
 #Robert Lalani 12.08.22
-  '''
-  G: (NOT MST OR match)
-    [{'1': 1, '2': 18, '3': 16, '4': 11},
-    {'0': 1, '2': 8, '3': 6, '4': 15},
-    {'0': 18, '1': 8, '3': 4, '4': 14},
-    {'0': 16, '1': 6, '2': 4, '4': 14},
-    {'0': 11, '1': 15, '2': 14, '3': 14}]
 
-    MatchedMSTree (MST + PerfectMatch)
-    [[0, 1, 0], [0, 4, 0], [1, 0, 0], [1, 2, 0], [1, 4, 0], [1, 3, 0], [2, 1, 0], [2, 5, 0], [3, 5, 0], [3, 1, 0], [4, 1, 0], [4, 0, 0], [5, 2, 0], [5, 3, 0]]
-  '''
 
   '''
    
@@ -21,14 +11,15 @@ Action:
 
     
 def find_eulerian_tour(Multigraph):
-    # find neigbours
+    
+    #Dictionary: Keys are the vertices in the Multigraph 
+    #Value is a list of neighbors 
     neighbours = {}
-    print('For')
     for edge in Multigraph:
-        if edge[0] not in neighbours:
+        if edge[0] not in neighbours: #Add neighbor v[u] :[ v[w] ] 
             neighbours[edge[0]] = []
 
-        if edge[1] not in neighbours:
+        if edge[1] not in neighbours: #Add neighbor v[w] :[ v[u] ]
             neighbours[edge[1]] = []
          
 
@@ -38,17 +29,25 @@ def find_eulerian_tour(Multigraph):
     print("Neighbours: ", neighbours)
 
     # finds the hamiltonian circuit
+     # Start at the first vertex (can be arbitrary) 
     start_vertex = Multigraph[0][0]
     print('start_vertex:', start_vertex)
+    
+    #Keeps track of the Eulerian Tour
     eulerian_circut = [neighbours[start_vertex][0]]
     while len(Multigraph) > 0:
+        #enumerates path
         for i, v in enumerate(eulerian_circut):
             if len(neighbours[v]) > 0:
                 break
-
+        #While vertex has neighbors 
         while len(neighbours[v]) > 0:
+            # Key : Selected Vertex , 
+            # Value: First Element in Neighbor list
             w = neighbours[v][0]
-
+             
+            # Remove the first value in {vertices : [ list of neighbors] }
+            # Delete edge from the Multigraph : [ list of neighbors] }
             remove_edge_from_Multigraph(Multigraph, v, w)
             print('Edge Removed:',v,w)
             del neighbours[v][(neighbours[v].index(w))]
@@ -57,11 +56,9 @@ def find_eulerian_tour(Multigraph):
             i += 1
   
             print(neighbours)
-            eulerian_circut.insert(i, w)
-        
-            
+            eulerian_circut.insert(i, w)  # Add vertex to tour path
 
-            v = w
+            v = w # Move selected vertex to w
 
     return eulerian_circut
 
